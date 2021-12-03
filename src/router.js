ac0c76b5b3b16f8c0f99bba50b443668b379d7e7
hungmanh02 <domanh462@gmail.com>
2021-12-04 00:08:48 +0700

    add 1 router shop

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import Features from "./pages/Features/Features";
 import Home from "./pages/home/Home";
+import Shop from "./pages/shop/Shop";
 
 const router = [
   { path: "/*", component: <Home /> },
   { path: "/feature", component: <Features /> },
+  { path: "/shop", component: <Shop /> },
 ];
 
 export default router;
